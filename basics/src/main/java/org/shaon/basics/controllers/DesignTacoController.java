b704cdad43783e6de6d79a85f1188a379b09bfed
shaon <shaon@cokreates.com>
2020-06-27 22:46:32 +0600

    validation added

--- basics/src/main/java/org/shaon/basics/controllers/DesignTacoController.java
+++ basics/src/main/java/org/shaon/basics/controllers/DesignTacoController.java
@@ -6,11 +6,13 @@ import org.shaon.basics.models.Ingredient.Type;
 import org.shaon.basics.models.Taco;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -56,12 +58,14 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
-//        Save the taco design...
-//        We'll do this in chapter 3
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+//      Save the taco design...
+//      We'll do this in chapter 3
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
-//        return "home";
     }
 
 
