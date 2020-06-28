65591a00b25799a7b4179ee0db8a468c95f94e60
shaon <shaon@cokreates.com>
2020-06-28 23:03:14 +0600

    configuration updated

--- basics/src/main/java/org/shaon/basics/BasicsApplication.java
+++ basics/src/main/java/org/shaon/basics/BasicsApplication.java
@@ -2,12 +2,19 @@ package org.shaon.basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BasicsApplication {
+public class BasicsApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicsApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
 	}
 
 }
